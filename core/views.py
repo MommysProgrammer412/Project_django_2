6e04c91d1e8023b48bf873257960dd54753b4b17
Semyon <semenb668@gmail.com>
2025-08-29 17:12:21 +0600

    lesson_65: - Стартовый неудачный вариант пагинатора - Не отображается

--- core/views.py
+++ core/views.py
@@ -101,6 +101,7 @@ class OrderListView(ListView):
     model = Order
     template_name = "orders_list.html"
     context_object_name = "orders"
+    paginate_by = 10
 
     def get_queryset(self):
         queryset = super().get_queryset()
